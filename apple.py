e3efe8856f7c7d4b1b8de8a9d028714f215c5582
Lucas Lopes <38164895+lucaslop@users.noreply.github.com>
2023-07-03 16:20:21 -0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,58 +1,114 @@
-from app_store_scraper import AppStore
-import csv
-import json
-from datetime import datetime
-from itunes_app_scraper.scraper import AppStoreScraper
+import requests
 import time
-# Read the app names and IDs from the txt file
-apps = {}
-with open('lista-ios-apps.txt', 'r') as file:
-    for line in file:
-        line = line.strip()
-        if line:
-            app_name, app_id = line.split('=')
-            apps[app_name.strip()] = app_id.strip()
+import json
+import csv
+from authlib.jose import jwt
+from datetime import datetime
+import os
+import unicodedata
 
-# Iterate over the apps and scrape reviews
-for app_name, app_id in apps.items():
-    app_info = AppStoreScraper()
-    ratings = app_info.get_app_ratings(app_id, 'br')
-    rating_avg = sum(int(rating) * count for rating, count in ratings.items()) / sum(ratings.values())
-    rating_avg = round(rating_avg)
-    app_detail = app_info.get_app_details(app_id, 'br')
-    version = app_detail['version']
+KEY_ID = ""
+ISSUER_ID = ""
+EXPIRATION_TIME = int(round(time.time() + (20.0 * 60.0)))  # 20 minutes timestamp
+PATH_TO_KEY = 'key.p8'
+PATH_TO_APPS_LIST = 'lista-ios-apps.txt'
 
-    aplicativo = AppStore(country="br", app_name="app", app_id=app_id)
-    aplicativo.review(how_many=10000)
+with open(PATH_TO_KEY, 'r') as f:
+    PRIVATE_KEY = f.read()
 
-    # Create a CSV file for each app
-    csv_file = f"{app_name}_reviews.csv"
-    with open(csv_file, 'w', newline='', encoding='utf-8') as file:
+header = {
+    "alg": "ES256",
+    "kid": KEY_ID,
+    "typ": "JWT"
+}
+
+payload = {
+    "iss": ISSUER_ID,
+    "exp": EXPIRATION_TIME,
+    "aud": "appstoreconnect-v1"
+}
+
+def create_jwt(header, payload, private_key):
+    token = jwt.encode(header, payload, private_key)
+    return token.decode()
+
+def write_csv(data, app_info):
+    app_name = app_info['data']['attributes']['name']
+    app_name_normalized = normalize_app_name(app_name)
+    csv_filename = f"{app_name_normalized}-ios.csv"
+
+    with open(csv_filename, 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['App Name', 'version', 'rating_avg', 'Author', 'Date', 'Title', 'Comment', 'Rating', 'Response'])
+        
+        if file.tell() == 0:
+            writer.writerow(['Aplicativo ID', 'Aplicativo', 'Nota', 'Título', 'Contéudo', 'Nome do Autor', 'Data', 'País'])
+        
+        sorted_data = sorted(data, key=lambda x: x['attributes']['createdDate'])
+        
+        for review in sorted_data:
+            review_id = review['id']
+            rating = review['attributes']['rating']
+            title = review['attributes']['title']
+            body = review['attributes']['body']
+            reviewer_nickname = review['attributes']['reviewerNickname']
+            created_date = datetime.strptime(review['attributes']['createdDate'], "%Y-%m-%dT%H:%M:%S%z").strftime("%d/%m/%Y")
+            territory = review['attributes']['territory']
+            
+            if 'response' in review.get('relationships', {}):
+                responded = True
+            else:
+                responded = False
+            
+            
+            writer.writerow([app_info['data']['id'], app_info['data']['attributes']['name'], rating, title, body, reviewer_nickname, created_date, territory])
 
-        for review in aplicativo.reviews:
-            date = review['date']
-            comment = review['review']
-            rating = review['rating']
-            title = review['title']
-            username = review['userName']
-            response = "Respondida" if 'developerResponse' in review else "Não respondida"
+def fetch_reviews(url, headers, app_info):
+    response = requests.get(url, headers=headers)
+    response_json = response.json()
+    
+    if 'data' in response_json:
+        data = response_json['data']
+        write_csv(data, app_info)
+    else:
+        print('No data found in the response.')
+    
+    if 'next' in response_json['links']:
+        next_url = response_json['links']['next']
+        return next_url
+    else:
+        return None
 
-            writer.writerow([app_name,version, rating_avg, username, date, title, comment, rating, response])
+def get_app_info(app_id, headers):
+    url = f'https://api.appstoreconnect.apple.com/v1/apps/{app_id}'
+    response = requests.get(url, headers=headers)
+    app_info = response.json()
+    return app_info
 
-    # Create a JSON file for each app
-    json_file = f"{app_name}_reviews.json"
-    reviews_data = {
-        'App Name': app_name,
-        'Versao': version,
-        'Estrelas': rating_avg,
-        'Reviews': aplicativo.reviews
-    }
-    with open(json_file, 'w', encoding='utf-8') as file:
-        # Convert datetime objects to strings
-        for review in reviews_data['Reviews']:
-            review['date'] = review['date'].strftime('%Y-%m-%d %H:%M:%S')
+def normalize_app_name(app_name):
+    normalized = unicodedata.normalize('NFKD', app_name).encode('ASCII', 'ignore').decode('utf-8')
+    return normalized.replace(" ", "_")
 
-        json.dump(reviews_data, file, ensure_ascii=False, indent=4)
-    time.sleep(15)
+def main():
+    if not os.path.isfile(PATH_TO_APPS_LIST):
+        print("O arquivo 'lista-ios-apps.txt' não existe.")
+        return
+
+    with open(PATH_TO_APPS_LIST, 'r') as apps_file:
+        for line in apps_file:
+            line = line.strip()
+            if line:
+                app_name, app_id = line.split('=')
+                app_id = app_id.strip()
+                
+                print(f"Coletando dados do aplicativo: {app_name}")
+                
+                token = create_jwt(header, payload, PRIVATE_KEY)
+                JWT = 'Bearer ' + token
+                HEAD = {'Authorization': JWT}
+                app_info = get_app_info(app_id, HEAD) 
+                URL = f'https://api.appstoreconnect.apple.com/v1/apps/{app_id}/customerReviews?limit=200'
+                while URL:
+                    URL = fetch_reviews(URL, HEAD, app_info)
+
+if __name__ == '__main__':
+    main()
